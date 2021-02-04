056cf21028ec6bf43c0542d3c1b6766a9c3e3e79
Antwan Tomatoes <atomatoe@su-1.kzn.21-school.ru>
2021-02-04 19:21:59 +0300

    upd time func

--- time.cpp
+++ time.cpp
@@ -6,14 +6,15 @@
 /*   By: atomatoe <atomatoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 17:39:26 by atomatoe          #+#    #+#             */
-/*   Updated: 2021/02/03 19:39:22 by atomatoe         ###   ########.fr       */
+/*   Updated: 2021/02/04 19:11:39 by atomatoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.hpp"
 
 // Date: Mon, 27 Jul 2009 12:28:53 GMT   вот так ответ должен быть
-// результат поместить либо в char* либо в std::string
+// результат поместить либо в char*
+// О структуре tm: https://www.opennet.ru/docs/RUS/glibc/glibc-17.html
 
 int main()
 {
@@ -24,21 +25,39 @@ int main()
     int sec_per_hour = 3600;
     int sec_per_min = 60;
     gettimeofday(&tv, &tz);
-    // printf("TimeZone-1 = %d\n", tz.tz_minuteswest);
-    // printf("TimeZone-2 = %d\n", tz.tz_dsttime);
-    // printf("TimeVal-3  = %lld\n", (long long) tv.tv_sec);
-    // printf("TimeVal-4  = %lld\n", (long long) tv.tv_usec);
+
+    
+
     long hms = tv.tv_sec % sec_per_day;
     hms += tz.tz_dsttime * sec_per_hour;
     hms -= tz.tz_minuteswest * sec_per_min;
     hms = (hms + sec_per_day) % sec_per_day;
 
-    tm.tm_mon = (tv.tv_sec - 1609448401); // получение месяца
-    tm.tm_year = 1970 + (tv.tv_sec / 31536000); // получение даты
-    tm.tm_hour = hms / sec_per_hour;
-    tm.tm_min = (hms % sec_per_hour) / sec_per_min;
-    tm.tm_sec = (hms % sec_per_hour) % sec_per_min;
+    tm.tm_year = 70 + (tv.tv_sec / 31536000); // получение года
+    tm.tm_hour = hms / sec_per_hour; // получение часа
+    tm.tm_min = (hms % sec_per_hour) / sec_per_min; // получение минуты
+    tm.tm_sec = (hms % sec_per_hour) % sec_per_min; // получение секунды
 
-    printf("Current local time: %d %d:%02d:%02d\n", tm.tm_year, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    char zon[4] = "GMT";
+    tm.tm_zone = zon;
+    long test = tv.tv_sec / 86400 ;
+    test = test % 7; // какой день недели
+    tm.tm_wday = test + 4;
+
+    char buf[200];
+    tm.tm_isdst = -1; // не используем(переход летнее время)
+    tm.tm_gmtoff = 0; // не используем
+    tm.tm_yday = 0; // не используем
+    tm.tm_mon = 0; // день
+    tm.tm_mday = 0; // месяц
+    // tm.tm_year = 0;
+    // tm.tm_hour = 0;
+    // tm.tm_wday = 0;
+    // tm.tm_min = 0;
+    // tm.tm_sec = 0;
+    strftime (buf, 200, "Date: %a, %d %b %Y %X %Z", &tm);
+    std::cout << buf << std::endl;
+    std::cout << "Date: Mon, 27 Jul 2009 12:28:53 GMT" << std::endl;
+    // printf("Date: %d %d:%02d:%02d\n", tm.tm_year, tm.tm_hour, tm.tm_min, tm.tm_sec);
     return 0;
 }
