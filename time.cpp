c3e569c7df69355eeeef1471df949be46b7a9760
Antwan Tomatoes <atomatoe@si-e3.kzn.21-school.ru>
2021-02-04 21:05:31 +0300

    upd time func

--- time.cpp
+++ time.cpp
@@ -6,7 +6,7 @@
 /*   By: atomatoe <atomatoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 17:39:26 by atomatoe          #+#    #+#             */
-/*   Updated: 2021/02/04 19:11:39 by atomatoe         ###   ########.fr       */
+/*   Updated: 2021/02/04 21:05:21 by atomatoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,54 @@ int main()
     int sec_per_min = 60;
     gettimeofday(&tv, &tz);
 
+    time_t tmp = tv.tv_sec;
+    int flag = 4;
+    int year = 0;
+    tmp = tmp - 31536000 - 31536000;
+    while(tmp > 31622400)
+    {
+        if(flag == 4)
+        {
+            tmp -= 31622400; 
+            flag = 0;
+        }
+        else
+        {
+            tmp -= 31536000;
+            flag++;
+        }
+        year++;
+    }
+    tmp -= 2678400;
+    tmp -= 86400;
+    tmp -= 86400;
+    tmp -= 86400;
+    tmp -= 86400;
+    std::cout << tmp << std::endl;
+    // tmp += 31536000; // переходим на 1 год вперед
+    // while(tmp < 0)
+    // {
+    //     2419200
+    // }
+    // tmp += 2678400;
+    // tmp += 2678400;
+    // tmp += 2678400;
+    // tmp += 2678400;
+    // tmp += 2678400;
+    // tmp += 2678400;
+    // tmp += 2592000;
+    // tmp += 2592000;
+    // tmp += 2592000;
+    // tmp += 2592000;
+    // tmp += 2419200;
+    // tmp -= 86400;
+    // tmp -= 86400;
+    // tmp -= 86400;
+    // tmp -= 86400;
+    // tmp -= 86400;
+    // tmp -= 86400;
+    // tmp -= 86400;
+    tm.tm_year = 70 + year; // получаем год
     
 
     long hms = tv.tv_sec % sec_per_day;
@@ -33,16 +81,15 @@ int main()
     hms -= tz.tz_minuteswest * sec_per_min;
     hms = (hms + sec_per_day) % sec_per_day;
 
-    tm.tm_year = 70 + (tv.tv_sec / 31536000); // получение года
     tm.tm_hour = hms / sec_per_hour; // получение часа
-    tm.tm_min = (hms % sec_per_hour) / sec_per_min; // получение минуты
-    tm.tm_sec = (hms % sec_per_hour) % sec_per_min; // получение секунды
-
+    tm.tm_min = (hms % sec_per_hour) / sec_per_min; // получение минут
+    tm.tm_sec = (hms % sec_per_hour) % sec_per_min; // получение секунд
     char zon[4] = "GMT";
     tm.tm_zone = zon;
     long test = tv.tv_sec / 86400 ;
     test = test % 7; // какой день недели
     tm.tm_wday = test + 4;
+
 
     char buf[200];
     tm.tm_isdst = -1; // не используем(переход летнее время)
@@ -50,14 +97,15 @@ int main()
     tm.tm_yday = 0; // не используем
     tm.tm_mon = 0; // день
     tm.tm_mday = 0; // месяц
+    strftime (buf, 200, "Date: %a, %d %b %Y %X %Z", &tm);
+    std::cout << buf << std::endl;
+    std::cout << "Date: Mon, 27 Feb 2021 12:28:53 GMT" << std::endl;
+    return 0;
+}
+
+
     // tm.tm_year = 0;
     // tm.tm_hour = 0;
     // tm.tm_wday = 0;
     // tm.tm_min = 0;
     // tm.tm_sec = 0;
-    strftime (buf, 200, "Date: %a, %d %b %Y %X %Z", &tm);
-    std::cout << buf << std::endl;
-    std::cout << "Date: Mon, 27 Jul 2009 12:28:53 GMT" << std::endl;
-    // printf("Date: %d %d:%02d:%02d\n", tm.tm_year, tm.tm_hour, tm.tm_min, tm.tm_sec);
-    return 0;
-}
