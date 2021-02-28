a681d02ddefe75684b32ac3ba056ac7dd49066be
Antwan Tomatoes <atomatoe@si-f1.kzn.21-school.ru>
2021-02-28 15:30:24 +0300

    autoindex bag fixed

--- includes/page_html.cpp
+++ includes/page_html.cpp
@@ -6,7 +6,7 @@
 /*   By: atomatoe <atomatoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 15:48:01 by atomatoe          #+#    #+#             */
-/*   Updated: 2021/02/24 17:50:03 by atomatoe         ###   ########.fr       */
+/*   Updated: 2021/02/28 15:30:05 by atomatoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,17 +49,14 @@ const char* Page_html::create_autoindex(std::string _directory, char *uri)
     _dir = opendir(_directory.c_str());
 
     _file = readdir(_dir);
-    _html += (char *)"<table style=\"width: 50%; border-collapse: collapse; margin-left: auto; margin-right: auto; height: 162px;\" border=\"1\" bgcolor=\"#9442ad\">\n<tbody>\n<tr style=\"height: 108px;\">\n<td bgcolor=\"#59bd59\" style=\"width: 50.25%; height: 108px; text-align: center;\">\n<p><strong>&#128149;<a href=\"https://github.com/atomatoe/webserv\">Welcome to Webserv!</a>&#128149;</strong></p>\n<p>Index of ";
+    _html = (char *)"<table style=\"width: 50%; border-collapse: collapse; margin-left: auto; margin-right: auto; height: 162px;\" border=\"1\" bgcolor=\"#9442ad\">\n<tbody>\n<tr style=\"height: 108px;\">\n<td bgcolor=\"#59bd59\" style=\"width: 50.25%; height: 108px; text-align: center;\">\n<p><strong>&#128149;<a href=\"https://github.com/atomatoe/webserv\">Welcome to Webserv!</a>&#128149;</strong></p>\n<p>Index of ";
     _html += _directory;
     _html += (char *)"</p>\n</td>\n<td style=\"width: 49.75%; height: 150px; text-align: center;\">\n<p><strong>Creators:</strong></p>\n<p>&#128125; <a href=\"https://github.com/atomatoe\"> atomatoe </a>&#128125;</p>\n<p><a><span>&#128123; <a href=\"https://github.com/Welease\"> welease </a>&#128123;</span></a></p>\n<p><a><span>&#129302; <a href=\"https://github.com/timurmug\">qtamaril</a> &#129302;</span></a></p>\n</td>\n</tr>\n";
     while((_file = readdir(_dir)))
     {
         _html += (char *)"<tr style=\"height: 18px;\"><td style=\"width: 50.25%; text-align: center; height: 30px;\" bgcolor=\"#1a7d99\"><a href=\"";
-        //std::cout << "dir autoindex = " << _directory << std::endl;
-        //std::cout << "file autoindex = " << _file->d_name << std::endl;
-        //std::cout << "uri autoindex = " << uri << std::endl;
         _html += uri;
-        if(strcmp(uri, "/") != 0)
+        if(uri[strlen(uri) - 1] != '/')
             _html += "/";
         _html += _file->d_name;
         _html += (char *)"\">";
@@ -73,6 +70,8 @@ const char* Page_html::create_autoindex(std::string _directory, char *uri)
     }
     _html += (char *)"</tbody>\n</table>";
     closedir(_dir);
+    std::cout << std::endl;
+    std::cout << _html << std::endl;
     return(_html.c_str());
 }
 
